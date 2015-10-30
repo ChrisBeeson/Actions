4c5958915042ac3595ebc241167c060465b6339f
Chris Beeson <chrisbeeson@me.com>
2015-10-31 07:08:27 +1100

    events working

---
+++ FilamentKit/WorkingWeekRule.swift
@@ -0,0 +1,31 @@
+//
+//  WorkingWeekRule.swift
+//  Filament
+//
+//  Created by Chris Beeson on 30/10/2015.
+//  Copyright © 2015 Andris Ltd. All rights reserved.
+//
+
+import Foundation
+import DateTools
+
+public struct WorkingWeekRule: Rule {
+    
+    // This rule sits the duration of an event.
+    // It allows the event to be shortened to a minimum duration if required.
+    
+    public var name: String { return "!!!" }
+    public var availableToNodeType:NodeType { return .None}
+    public var conflictingRules: [Rule]? { return nil }
+    public var options: RoleOptions { get { return RoleOptions.RequiresInterestWindow } }
+    
+    // Rule inputs
+    public var inputDate: NSDate?
+    public var interestPeriod: DTTimePeriod?
+    
+    //custom
+    
+    public var workingDay: DTTimePeriod?
+    public var breaks: [DTTimePeriod]?     //default lunch 12.30 -> 13.30
+    
+}
