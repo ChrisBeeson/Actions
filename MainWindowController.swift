aae90b2ec851db077001b30720bec2a8f4a90c62
Chris Beeson <Chrisbeeson@me.com>
2016-01-23 19:16:24 +1100

    upload to gitlab

---
+++ MainWindowController.swift
@@ -0,0 +1,26 @@
+//
+//  WindowController.swift
+//  Filament
+//
+//  Created by Chris Beeson on 1/11/2015.
+//  Copyright © 2015 Andris Ltd. All rights reserved.
+//
+
+import Cocoa
+
+class MainWindowController: NSWindowController {
+    
+    
+    override func awakeFromNib() {
+        
+        super.awakeFromNib()
+        self.window!.titleVisibility = NSWindowTitleVisibility.Hidden
+    }
+    
+    
+    @IBAction func segmentedControlAction(sender: NSSegmentedControl) {
+        
+        print("Segmented Control Changed")
+    }
+    
+}
