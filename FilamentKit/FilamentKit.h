3f324dc9ca1b79b4148ba61abb708c8c1beabaf2
Chris Beeson <Chrisbeeson@me.com>
2016-01-23 20:57:01 +1100

    fixed all issues after moving to gitlab

---
+++ FilamentKit/FilamentKit.h
@@ -0,0 +1,19 @@
+//
+//  FilamentKit.h
+//  FilamentKit
+//
+//  Created by Chris Beeson on 5/09/2015.
+//  Copyright (c) 2015 Andris Ltd. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for FilamentKit.
+FOUNDATION_EXPORT double FilamentKitVersionNumber;
+
+//! Project version string for FilamentKit.
+FOUNDATION_EXPORT const unsigned char FilamentKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FilamentKit/PublicHeader.h>
+
+
