3a2f70d634ac320ecbe01dcdc55f7e3487c187de
Chris Beeson <chrisbeeson@me.com>
2016-02-10 15:09:58 +1100

    + button animates in and out

--- FilamentKit/AddNewNodeCollectionViewItem.swift
+++ FilamentKit/AddNewNodeCollectionViewItem.swift
@@ -10,8 +10,41 @@ import Foundation
 
 class AddNewNodeCollectionViewItem : NSCollectionViewItem {
     
+    @IBOutlet weak var plusButton: NSButton!
+    
+    
+    override func viewDidLoad() {
+        
+        let tracking = NSTrackingArea(rect: self.view.frame, options: [.MouseEnteredAndExited,.ActiveInActiveApp] , owner: self, userInfo: nil)
+        self.view.addTrackingArea(tracking)
+         plusButton.alphaValue = 0.0
+    }
+    
+    
+    
+    
     @IBAction func plusButtonPressed(sender: AnyObject) {
         
+
+    }
+    
+
+    
+     override func mouseEntered(theEvent: NSEvent) {
+
+        NSAnimationContext.beginGrouping()
+        NSAnimationContext.currentContext().duration  = 0.5
+        plusButton.animator().alphaValue = 1.0
+        NSAnimationContext.endGrouping()
+    }
+    
+    override func mouseExited(theEvent: NSEvent) {
+        
+        NSAnimationContext.beginGrouping()
+        NSAnimationContext.currentContext().duration  = 0.5
+        plusButton.animator().alphaValue = 0.0
+        NSAnimationContext.endGrouping()
     }
     
 }
+
