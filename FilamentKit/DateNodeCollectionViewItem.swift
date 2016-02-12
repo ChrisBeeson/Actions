de313046f2d8c26808a7b1014a3fb4fd877508df
Chris Beeson <chrisbeeson@me.com>
2016-02-12 11:37:27 +1100

    calendarView pops correctly

--- FilamentKit/DateNodeCollectionViewItem.swift
+++ FilamentKit/DateNodeCollectionViewItem.swift
@@ -7,12 +7,54 @@
 //
 
 import Foundation
+import MLCalendarView
 
-class DateNodeCollectionViewItem : NSCollectionViewItem {
+public class DateNodeCollectionViewItem : NSCollectionViewItem, MLCalendarViewDelegate {
     
     @IBOutlet weak var month: NSTextField!
     @IBOutlet weak var day: NSTextField!
     @IBOutlet weak var dayString: NSTextField!
     @IBOutlet weak var time: NSTextField!
+    
+   
+    var popover: NSPopover?
+    var calView: MLCalendarView?
+    
+    /*
+    required public init(coder: NSCoder) {
+        super.init(coder: coder)!
+    }
+    
+   */
+    
+    override public func mouseDown(theEvent: NSEvent) {
+        
+        // display calendar popup
+        
+        if popover == nil {
+            popover = NSPopover()
+            popover!.animates = true
+            popover!.behavior = .Transient
+            popover!.appearance = NSAppearance(named: NSAppearanceNameAqua)
+            
+            if calView == nil {
+                calView = MLCalendarView()
+                calView!.delegate = self
+            }
+            
+            popover!.contentViewController = calView
+        }
+        
+        popover?.showRelativeToRect(self.dayString!.frame, ofView: self.view, preferredEdge:.MinX )
+    }
+    
+
+    
+    
+    public func didSelectDate(selectedDate: NSDate!) {
+    }
+    
+    
+
     
 }
