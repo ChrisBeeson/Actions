d2e7730d759c319f741a163d068c1f82f251e3ba
Chris Beeson <chrisbeeson@me.com>
2016-02-25 07:32:39 +1100

    renamed

---
+++ FilamentKit/EventDurationWithMinimumDuration.swift
@@ -0,0 +1,55 @@
+//
+//  EventDuration.swift
+//  Filament
+//
+//  Created by Chris Beeson on 25/09/2015.
+//  Copyright © 2015 Andris Ltd. All rights reserved.
+//
+
+import Foundation
+import DateTools
+
+ class EventDurationWithMinimumDuration : Rule, NSCoding  {
+    
+    // This rule sits the duration of an event.
+    // It allows the event to be shortened to a minimum duration if required.
+    
+     override var name: String { return "Minimum Duration" }
+     override var availableToNodeType:NodeType { return .Action}
+     override var conflictingRules: [Rule]? { return nil }
+    
+     override init() {
+        super.init()
+    }
+    
+    // Specific user controls
+    
+     var duration = TimeSize(unit: .Minute, amount: 30)
+     var minDuration = TimeSize(unit: .Minute, amount: 15)
+    
+    
+    // Rule out values
+    
+     override var eventMinDuration: TimeSize? { get { return minDuration } }
+     override var eventDuration: TimeSize? { get { return duration } }
+    
+    
+    // MARK: NSCoding
+    
+    private struct SerializationKeys {
+        static let duration = "duration"
+        static let minDuration = "minDuration"
+    }
+    
+     required init?(coder aDecoder: NSCoder) {
+        
+        duration = aDecoder.decodeObjectForKey(SerializationKeys.duration) as! TimeSize
+        minDuration = aDecoder.decodeObjectForKey(SerializationKeys.minDuration) as! TimeSize
+    }
+    
+     func encodeWithCoder(aCoder: NSCoder) {
+        
+        aCoder.encodeObject(duration, forKey:SerializationKeys.duration)
+        aCoder.encodeObject(minDuration, forKey:SerializationKeys.minDuration)
+    }
+}
