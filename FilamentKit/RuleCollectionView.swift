b3dc0b3bff6c9b3ba8aa713f5e815eb0e864cced
Chris Beeson <Chrisbeeson@me.com>
2016-03-05 08:07:05 +1100

    pill size fixed

--- FilamentKit/RuleCollectionView.swift
+++ FilamentKit/RuleCollectionView.swift
@@ -92,8 +92,8 @@ public class RuleCollectionView : NSCollectionView, NSCollectionViewDataSource, 
     public func collectionView(collectionView: NSCollectionView, layout collectionViewLayout: NSCollectionViewLayout, sizeForItemAtIndexPath indexPath: NSIndexPath) -> NSSize {
         
             let string:NSString = rulePresenters![indexPath.item].name as NSString
-            let size: CGSize = string.sizeWithAttributes([NSFontAttributeName: NSFont.systemFontOfSize(12.0, weight:NSFontWeightRegular) ])
-            return NSSize(width: size.width + 30, height: 16)
+            let size: CGSize = string.sizeWithAttributes([NSFontAttributeName: NSFont.systemFontOfSize(13.0, weight:NSFontWeightRegular) ])
+            return NSSize(width: size.width + 33, height: 16)
     }
     
     public func collectionView(collectionView: NSCollectionView, layout collectionViewLayout: NSCollectionViewLayout, minimumLineSpacingForSectionAtIndex section: Int) -> CGFloat {
