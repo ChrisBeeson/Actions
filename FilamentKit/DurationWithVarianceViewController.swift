5be9a7c4a1b255a93dfe36d17abba9c1e74c2c16
Chris Beeson <Chrisbeeson@me.com>
2016-03-10 17:23:53 +1100

    +/- duration replaced with popover

--- FilamentKit/DurationWithVarianceViewController.swift
+++ FilamentKit/DurationWithVarianceViewController.swift
@@ -9,50 +9,10 @@
 import Foundation
 import DateTools
 
-class DurationWithVarianceViewController : RuleViewController, NSComboBoxDelegate, RulePresenterDelegate  {
-    
-    @IBOutlet weak var durationUnitComboBox: NSComboBox!
-    @IBOutlet weak var variationUnitComboBox: NSComboBox!
-    
+class DurationWithVarianceViewController : RuleViewController, RulePresenterDelegate  {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        refreshComboBoxes()
     }
     
-
-    func refreshComboBoxes() {
-        
-        let presenter = rulePresenter as! TransitionDurationWithVarianceRulePresenter
-        
-        durationUnitComboBox.selectItemAtIndex(Int(presenter.durationUnit))
-        durationUnitComboBox.stringValue = durationUnitComboBox.objectValueOfSelectedItem as! String
-        
-        variationUnitComboBox.selectItemAtIndex(Int(presenter.varianceUnit))
-        variationUnitComboBox.stringValue = variationUnitComboBox.objectValueOfSelectedItem as! String
-    }
-    
-    
-    func comboBoxSelectionDidChange(notification: NSNotification) {
-        
-        let combobox = notification.object as! NSComboBox
-        let presenter = rulePresenter as! TransitionDurationWithVarianceRulePresenter
-          
-            switch combobox.tag {
-                
-            case 1:
-                presenter.durationUnit = UInt(combobox.indexOfSelectedItem)
-                
-            case 2:
-                 presenter.varianceUnit = UInt(combobox.indexOfSelectedItem)
-                
-            default: break
-            }
-    }
-    
-    func rulePresenterDidChangeContent(presenter: RulePresenter) {
-        
-        refreshComboBoxes()
-    }
 }
